6b076a9283354baf6e756556a92c65d0965a6ad7
Daniel Taquez <daniel.taquez@du.edu>
2019-09-11 21:12:35 -0600

    sql values fix

--- bamazon.sql
+++ bamazon.sql
@@ -2,40 +2,41 @@ DROP DATABASE IF EXISTS bamazon_db;
 CREATE DATABASE bamazon_db;
 USE bamazon_db;
 
-CREATE TABLE product (
-  item_id VARCHAR(30) NOT NULL,
-  product_name VARCHAR(30),
-  department_name VARCHAR(30),
-  item_price INTEGER(10),
-  stock_q BOOLEAN NOT NULL
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(30) NOT NULL,
+  department_name VARCHAR(30) NOT NULL,
+  item_price INTEGER(10) DEFAULT 0,
+  StockQuantity INT DEFAULT 0,
+  PRIMARY KEY(id)
 );
 
-INSERT INTO product (item_id, product_name, department_name, item_price,stock_q)
-VALUES ("phone", "iphone", "denver", 1000, TRUE);
+INSERT INTO product (productName, departmentName, itemPrice,stockQuantity)
+VALUES ('Brooks Launch 2', 'Running Shoes', 99, 12);
 
-INSERT INTO product (item_id, product_name, department_name, item_price,stock_q)
-VALUES ("phone2", "samsung", "denver", 800, TRUE);
+INSERT INTO product (productName, departmentName, itemPrice,stockQuantity)
+VALUES ('Brooks Launch 2', 'Running Shoes', 99, 12);
 
-INSERT INTO product (item_id, product_name, department_name, item_price,stock_q)
-VALUES ("phone3", "Google", "denver", 600, TRUE);
+INSERT INTO product (productName, departmentName, itemPrice,stockQuantity)
+VALUES ("phone3", "google", "denver", 600, TRUE);
 
-INSERT INTO product (item_id, product_name, department_name, item_price,stock_q)
-VALUES ("phone4", "Blackberry", "denver", 190, TRUE);
+INSERT INTO product (productName, departmentName, itemPrice,stockQuantity)
+VALUES ("phone4", "blackberry", "denver", 190, TRUE);
 
-INSERT INTO product (item_id, product_name, department_name, item_price,stock_q)
+INSERT INTO product (productName, departmentName, itemPrice,stockQuantity)
 VALUES ("phone5", "motorolla", "denver", 250, TRUE);
 
-INSERT INTO product (item_id, product_name, department_name, item_price,stock_q)
+INSERT INTO product (productName, departmentName, itemPrice,stockQuantity)
 VALUES ("phone6", "lg", "denver", 150, TRUE);
 
-INSERT INTO product (item_id, product_name, department_name, item_price,stock_q)
+INSERT INTO product (productName, departmentName, itemPrice,stockQuantity)
 VALUES ("phone7", "sony", "denver", 300, TRUE);
 
-INSERT INTO product (item_id, product_name, department_name, item_price,stock_q)
+INSERT INTO product (productName, departmentName, itemPrice,stockQuantity)
 VALUES ("phone8", "htc", "denver", 230, TRUE);
 
-INSERT INTO product (item_id, product_name, department_name, item_price,stock_q)
+INSERT INTO product (productName, departmentName, itemPrice,stockQuantity)
 VALUES ("phone9", "oneplus", "denver", 790, TRUE);
 
-INSERT INTO product (item_id, product_name, department_name, item_price,stock_q)
+INSERT INTO product (productName, departmentName, itemPrice,stockQuantity)
 VALUES ("phonex", "nokia", "denver", 50, TRUE);
