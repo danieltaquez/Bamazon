a3c5f94c102baf7373469a7fb76175a1f57f26cd
Daniel Taquez <daniel.taquez@du.edu>
2019-09-11 20:09:13 -0600

    Data points SQL

--- bamazon.sql
+++ bamazon.sql
@@ -2,22 +2,40 @@ DROP DATABASE IF EXISTS bamazon_db;
 CREATE DATABASE bamazon_db;
 USE bamazon_db;
 
-CREATE TABLE producta (
+CREATE TABLE product (
   item_id VARCHAR(30) NOT NULL,
   product_name VARCHAR(30),
   department_name VARCHAR(30),
   item_price INTEGER(10),
-  stock_q INTEGER(10)
+  stock_q BOOLEAN NOT NULL
 );
 
-INSERT INTO people (name, has_pet, pet_name, pet_age)
-VALUES ("Ahmed", TRUE, "Rockington", 100);
+INSERT INTO product (item_id, product_name, department_name, item_price,stock_q)
+VALUES ("phone", "iphone", "denver", 1000, TRUE);
 
-INSERT INTO people (name, has_pet, pet_name, pet_age)
-VALUES ("Ahmed", TRUE, "Rockington", 100);
+INSERT INTO product (item_id, product_name, department_name, item_price,stock_q)
+VALUES ("phone2", "samsung", "denver", 800, TRUE);
 
-INSERT INTO people (name, has_pet, pet_name, pet_age)
-VALUES ("Jacob", TRUE, "Misty", 10);
+INSERT INTO product (item_id, product_name, department_name, item_price,stock_q)
+VALUES ("phone3", "Google", "denver", 600, TRUE);
 
-INSERT INTO people (name, has_pet)
-VALUES ("Peter", false);
+INSERT INTO product (item_id, product_name, department_name, item_price,stock_q)
+VALUES ("phone4", "Blackberry", "denver", 190, TRUE);
+
+INSERT INTO product (item_id, product_name, department_name, item_price,stock_q)
+VALUES ("phone5", "motorolla", "denver", 250, TRUE);
+
+INSERT INTO product (item_id, product_name, department_name, item_price,stock_q)
+VALUES ("phone6", "lg", "denver", 150, TRUE);
+
+INSERT INTO product (item_id, product_name, department_name, item_price,stock_q)
+VALUES ("phone7", "sony", "denver", 300, TRUE);
+
+INSERT INTO product (item_id, product_name, department_name, item_price,stock_q)
+VALUES ("phone8", "htc", "denver", 230, TRUE);
+
+INSERT INTO product (item_id, product_name, department_name, item_price,stock_q)
+VALUES ("phone9", "oneplus", "denver", 790, TRUE);
+
+INSERT INTO product (item_id, product_name, department_name, item_price,stock_q)
+VALUES ("phonex", "nokia", "denver", 50, TRUE);
